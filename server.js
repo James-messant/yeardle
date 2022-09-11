e9540cb2120f368a71a2ab25318e0ea492700662
James-messant <jmessant@hotmail.com>
2022-09-11 19:08:21 +0100

    configured node

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static("public"));
+
+//app.set('view engine', 'ejs');
+
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+  app.listen(process.env.PORT || 3000, () =>{
+    console.log("server is running on port 3000.");
+  });
